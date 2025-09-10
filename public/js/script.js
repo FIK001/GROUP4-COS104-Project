b3a5cdfb7b619e66ad2d9d8955eb310b34f514f6
FIK001 <samuelfikayomi08@gmail.com>
2025-09-10 10:16:14 +0100

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -216,4 +216,11 @@ function initAnimationsAndScroll() {
     // executeStartAnimations(),
     fadeInOnScroll();
 }
+const hamburger = document.getElementById("hamburger");
+const mobileNav = document.getElementById("mobileNav");
+
+hamburger.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
+
 
